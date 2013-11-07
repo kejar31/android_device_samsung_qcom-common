4802a93e4fe02bd594a9786d0cdb12ef3de0787b
David Ferguson <ferguson.david@gmail.com>
2013-11-07 10:17:59 -0500

    qcom: allow properly querying of battery capacity
      * override the battery capacity sysfs path
      * requires patch to system/core/healthd/Android.mk
    
    Change-Id: I807745028bafa15f2267a37206045e96d1c40e3b
    
    Conflicts:
    	BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -62,4 +62,9 @@ BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charg
 BOARD_BATTERY_DEVICE_NAME := "battery"
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/samsung/qcom-common
+BOARD_HARDWARE_CLASS := hardware/samsung/cmhw
+
+# Workaround to allow us to have a local libhealthd
+TARGET_PROVIDES_LIBHEALTHD := true
+
 
